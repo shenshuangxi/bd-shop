61ce6399983ee731b91b4517a90f6497f8322c1f
jojozoo <jojozoo@vip.qq.com>
2017-04-25 01:10:27 +0800

    该死的输入法

--- b2c-simple-server/src/main/java/com/bigdatan/b2c/entity/AdPosition.java
+++ b2c-simple-server/src/main/java/com/bigdatan/b2c/entity/AdPosition.java
@@ -1,7 +1,12 @@
 package com.bigdatan.b2c.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -9,71 +14,32 @@ import java.util.Date;
  *
  * 广告位置实体类
  */
+@Data
 public class AdPosition implements Serializable {
+    /**
+     * 广告位置id，主键，自增1
+     */
     private Integer adPositionId;
+    /**
+     * 广告名称
+     */
     private String name;
+    /**
+     * 位置
+     */
     private String position;
+    /**
+     * 尺寸
+     */
     private String measure;
-    private Byte state;
+    /**
+     * 状态，1开启 2关闭
+     */
+    private Integer state;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
-    public Integer getAdPositionId() {
-        return adPositionId;
-    }
-
-    public void setAdPositionId(Integer adPositionId) {
-        this.adPositionId = adPositionId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public String getMeasure() {
-        return measure;
-    }
-
-    public void setMeasure(String measure) {
-        this.measure = measure;
-    }
-
-    public Byte getState() {
-        return state;
-    }
-
-    public void setState(Byte state) {
-        this.state = state;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 
 }
